2fa418000ee2b77c8fc2a0ebf07c743656bec654
hirata0517masato <hirata0517masato@yahoo.co.jp>
2018-02-26 16:47:37 +0900

    リスタートボタンの追加
    画面上のボタンを押すと再読み込みが行われるようになった

---
+++ MoveRobot/Assets/RestartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; 
+
+public class RestartButton : MonoBehaviour {
+
+	void Start () {
+    	
+		transform.position = new Vector3(90, Screen.height-25, 0f);
+	}
+
+	/// ボタンをクリックした時の処理
+  	public void OnClick() {
+    	//Application.LoadLevel( Application.loadedLevelName );
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  	}
+}
+
+
